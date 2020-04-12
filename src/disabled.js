aef5e97e516669cb2dbef8149a06d4e1c4b81d93
Yamil Friadenrich <yamiljf@gmail.com>
2020-04-12 10:53:37 +0700

    test

--- src/disabled.js
+++ src/disabled.js
@@ -5,8 +5,6 @@ require("./current_file").file(__filename);
  * unsafe Regex
  */
 
-console.log('password', '123SDFrkh%^%@sfdk.');
-
 child.exec(com);
 
 var unsafeRegex = /(x+x+)+y/; // guardrails-disable-line
