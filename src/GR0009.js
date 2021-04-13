f240dbf3def5492fe8f5417fc47c8d189a7ec92a
Yamil Friadenrich <yamiljf@gmail.com>
2021-04-13 12:56:56 +0700

    Update GR0009.js

--- src/GR0009.js
+++ src/GR0009.js
@@ -1,10 +1,7 @@
 require('./current_file').file(__filename);
 
-// test 1
-// add
-// 3
-// lines
-
 let com = '/bin/echo lol';
 var child = require('child_process'); 
 child.exec(com);
+
+// test 2
