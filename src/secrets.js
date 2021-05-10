f9db4cbf7b4b330ab2bf6c048a0b95856fbd54e3
Yamil Friadenrich <yamiljf@gmail.com>
2021-05-10 14:26:15 +0700

    Update secrets.js

--- src/secrets.js
+++ src/secrets.js
@@ -1,6 +1,6 @@
 require('./current_file').file(__filename);
 
 let aLongSecret =
-  'eafc0093a2b9a14e86ed8d3cb43c31f8e58147728f489550773545566bc7dda5\n';
+  'eafc0093a2b9a14e86ed8d3cb43c31f8e58147728f489550773545566bc7dda5';
 
 let password = 'IShouldNotBeFound';
